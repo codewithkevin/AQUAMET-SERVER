7b3c656804593123dd1a889b030fa5a140bcecb2
Kelvin Osei <oseikelvin709@gmail.com>
2023-05-31 22:52:38 +0000

    And login Controller for user website

--- src/controllers/Users/website/usersController.js
+++ src/controllers/Users/website/usersController.js
@@ -21,4 +21,37 @@ const createUserAccount = async (req, res) => {
   res.status(200).send({ result, token });
 };
 
+//Login In User Account
+const loginUserAccount = async (req, res) => {
+  const { error } = loginValidate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid Credentials");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid Credentials");
+
+  const token = user.generateAuthToken();
+
+  res.status(200).send({ token });
+};
+
+//Get User credentials
+const getUserAccount = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).send("User Not Found");
+
+  res.status(200).send(user);
+};
+
+function loginValidate(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
 export { createUserAccount };
