1b69b8c9a3b04674cf83cc125e82c3342c41ff2a
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-11 15:40:31 +0000

    Fixed error in config folder

--- src/startup/config.js
+++ src/startup/config.js
@@ -2,8 +2,10 @@ import config from "config";
 
 export default function () {
   const nodeEnv = config.util.getEnv("NODE_ENV");
+  const jwt = config.get("jwtPrivateKey");
 
   console.log("Current environment:", nodeEnv);
+    console.log("Current jwtPrivateKey:", jwt);
   if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
