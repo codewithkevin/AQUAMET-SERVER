828d699ba43e3bdee41ae8610b27b25be3101a44
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-14 13:04:45 +0000

    Added a test function to check code snet

---
+++ src/test/unit/sendmailOtp.test.js
@@ -0,0 +1,28 @@
+import sendConfirmationCode from "../../functions/Gmail/otp.js";
+
+jest.mock("console", () => ({
+  log: jest.fn(),
+}));
+
+describe("sendConfirmationCode", () => {
+  it("should send an email with a confirmation code", async () => {
+    const email = "test@example.com";
+    const confirmationCode = "123456";
+
+    await sendConfirmationCode(email, confirmationCode);
+
+    jest.spyOn(console, "log");
+    await Promise.resolve(); 
+    console.log("Email sent: OK");
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log.mock.calls[0][0]).toEqual("Email sent: OK");
+  });
+
+  it("should throw an error if the email address is invalid", async () => {
+    try {
+      await sendConfirmationCode("invalid_email", "123456");
+    } catch (error) {
+      expect(error.message).toEqual("Invalid email address");
+    }
+  });
+});
