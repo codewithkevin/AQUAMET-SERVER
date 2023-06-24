df17ca4a83c648453ca832a875eb897179355e7a
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-24 09:55:19 +0000

    Added Changes to confirm email and welcome email template

--- src/template/gmail/confirmCode.js
+++ src/template/gmail/confirmCode.js
@@ -2,232 +2,34 @@ const Templates = {
   HTML: (data) => {
     const { confirmationCode } = data;
     return `
-      <!DOCTYPE html>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+        <html lang="en">
 
-      <html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
-      <head>
-      <title></title>
-      <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-      <meta content="width=device-width, initial-scale=1.0" name="viewport"/><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
-      <style>
-              * {
-                  box-sizing: border-box;
-              }
-      
-              body {
-                  margin: 0;
-                  padding: 0;
-              }
-      
-              a[x-apple-data-detectors] {
-                  color: inherit !important;
-                  text-decoration: inherit !important;
-              }
-      
-              #MessageViewBody a {
-                  color: inherit;
-                  text-decoration: none;
-              }
-      
-              p {
-                  line-height: inherit
-              }
-      
-              .desktop_hide,
-              .desktop_hide table {
-                  mso-hide: all;
-                  display: none;
-                  max-height: 0px;
-                  overflow: hidden;
-              }
-      
-              .image_block img+div {
-                  display: none;
-              }
-      
-              @media (max-width:520px) {
-                  .desktop_hide table.icons-inner {
-                      display: inline-block !important;
-                  }
-      
-                  .icons-inner {
-                      text-align: center;
-                  }
-      
-                  .icons-inner td {
-                      margin: 0 auto;
-                  }
-      
-                  .image_block img.big,
-                  .row-content {
-                      width: 100% !important;
-                  }
-      
-                  .mobile_hide {
-                      display: none;
-                  }
-      
-                  .stack .column {
-                      width: 100%;
-                      display: block;
-                  }
-      
-                  .mobile_hide {
-                      min-height: 0;
-                      max-height: 0;
-                      max-width: 0;
-                      overflow: hidden;
-                      font-size: 0px;
-                  }
-      
-                  .desktop_hide,
-                  .desktop_hide table {
-                      display: table !important;
-                      max-height: none !important;
-                  }
-              }
-          </style>
-      </head>
-      <body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
-      <table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%">
-      <tbody>
-      <tr>
-      <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;" width="100%">
-      <tbody>
-      <tr>
-      <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
-      <tbody>
-      <tr>
-      <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
-      <table border="0" cellpadding="0" cellspacing="0" class="image_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad" style="padding-bottom:10px;width:100%;padding-right:0px;padding-left:0px;">
-      <div align="center" class="alignment" style="line-height:10px"><img alt="your-logo" src="https://aquamet-website.vercel.app/static/media/logo512.9a3ca2850c06c503ecfe.png" style="display: block; height: auto; border: 0; width: 125px; max-width: 100%;" title="your-logo" width="125"/></div>
-      </td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;" width="100%">
-      <tbody>
-      <tr>
-      <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; width: 500px;" width="500">
-      <tbody>
-      <tr>
-      <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 20px; padding-top: 15px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
-      <table border="0" cellpadding="0" cellspacing="0" class="image_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad" style="padding-bottom:5px;padding-left:5px;padding-right:5px;width:100%;">
-      <div align="center" class="alignment" style="line-height:10px"><img alt="reset-password" class="big" src="https://d1oco4z2z1fhwp.cloudfront.net/templates/default/2966/gif-resetpass.gif" style="display: block; height: auto; border: 0; width: 350px; max-width: 100%;" title="reset-password" width="350"/></div>
-      </td>
-      </tr>
-      </table>
-      <table border="0" cellpadding="0" cellspacing="0" class="heading_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad" style="text-align:center;width:100%;">
-      <h1 style="margin: 0; color: #393d47; direction: ltr; font-family: Tahoma, Verdana, Segoe, sans-serif; font-size: 25px; font-weight: normal; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0;"><strong>Confirm your email?</strong></h1>
-      </td>
-      </tr>
-      </table>
-      <table border="0" cellpadding="10" cellspacing="0" class="text_block block-3" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
-      <tr>
-      <td class="pad">
-      <div style="font-family: Tahoma, Verdana, sans-serif">
-      <div class="" style="font-size: 12px; mso-line-height-alt: 18px; color: #393d47; line-height: 1.5; font-family: Tahoma, Verdana, Segoe, sans-serif;">
-      <p style="margin: 0; font-size: 12px; mso-line-height-alt: 18px;"> </p>
-      </div>
-      </div>
-      </td>
-      </tr>
-      </table>
-      <table border="0" cellpadding="15" cellspacing="0" class="button_block block-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad">
-      <div align="center" class="alignment"><!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="www.yourwebsite.com" style="height:58px;width:149px;v-text-anchor:middle;" arcsize="35%" strokeweight="0.75pt" strokecolor="#FFC727" fillcolor="#ffc727"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#393d47; font-family:Tahoma, Verdana, sans-serif; font-size:18px"><![endif]--><a href="www.yourwebsite.com" style="text-decoration:none;display:inline-block;color:#393d47;background-color:#ffc727;border-radius:20px;width:auto;border-top:1px solid #FFC727;font-weight:undefined;border-right:1px solid #FFC727;border-bottom:1px solid #FFC727;border-left:1px solid #FFC727;padding-top:10px;padding-bottom:10px;font-family:Tahoma, Verdana, Segoe, sans-serif;font-size:18px;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank"><span style="padding-left:50px;padding-right:50px;font-size:18px;display:inline-block;letter-spacing:normal;"><span dir="ltr" style="word-break: break-word; line-height: 36px;">${confirmationCode}</span></span></a><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></div>
-      </td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-3" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;" width="100%">
-      <tbody>
-      <tr>
-      <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
-      <tbody>
-      <tr>
-      <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
-      <table border="0" cellpadding="0" cellspacing="0" class="empty_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad">
-      <div></div>
-      </td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tbody>
-      <tr>
-      <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
-      <tbody>
-      <tr>
-      <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
-      <table border="0" cellpadding="0" cellspacing="0" class="icons_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="pad" style="vertical-align: middle; color: #9d9d9d; font-family: inherit; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
-      <table cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-      <tr>
-      <td class="alignment" style="vertical-align: middle; text-align: center;"><!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
-      <!--[if !vml]><!-->
-      <table cellpadding="0" cellspacing="0" class="icons-inner" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;"><!--<![endif]-->
-      <tr>
-      <td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="https://www.designedwithbee.com/" style="text-decoration: none;" target="_blank"><img align="center" alt="Designed with BEE" class="icon" height="32" src="images/bee.png" style="display: block; height: auto; margin: 0 auto; border: 0;" width="34"/></a></td>
-      <td style="font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 15px; color: #9d9d9d; vertical-align: middle; letter-spacing: undefined; text-align: center;"><a href="https://www.designedwithbee.com/" style="color: #9d9d9d; text-decoration: none;" target="_blank">Designed with BEE</a></td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table>
-      </td>
-      </tr>
-      </tbody>
-      </table><!-- End -->
-      </body>
-      </html>
+            <head></head>
+
+            <body style="background-color:#ffffff;font-family:HelveticaNeue,Helvetica,Arial,sans-serif">
+                <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:37.5em;background-color:#ffffff;border:1px solid #eee;border-radius:5px;box-shadow:0 5px 10px rgba(20,50,70,.2);margin-top:20px;width:360px;margin:0 auto;padding:68px 0 130px">
+                <tr style="width:100%">
+                    <td><img alt="Plaid" src="https://aquamet-website.vercel.app/static/media/logo512.9a3ca2850c06c503ecfe.png" width="212" height="130" style="display:block;outline:none;border:none;text-decoration:none;margin:0 auto" />
+                    <p style="font-size:11px;line-height:16px;margin:16px 8px 8px 8px;color:#0a85ea;font-weight:700;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;height:16px;letter-spacing:0;text-transform:uppercase;text-align:center">Verify Your Identity</p>
+                    <h1 style="color:#000;display:inline-block;font-family:HelveticaNeue-Medium,Helvetica,Arial,sans-serif;font-size:20px;font-weight:500;line-height:24px;margin-bottom:0;margin-top:0;text-align:center">Enter the following code to finish linking Aquamet.</h1>
+                    <table style="background:rgba(0,0,0,.05);border-radius:4px;margin:16px auto 14px;vertical-align:middle;width:280px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+                        <tbody>
+                        <tr>
+                            <td>
+                            <p style="font-size:32px;line-height:40px;margin:0 auto;color:#000;display:inline-block;font-family:HelveticaNeue-Bold;font-weight:700;letter-spacing:6px;padding-bottom:8px;padding-top:8px;width:100%;text-align:center">${confirmationCode}</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </td>
+                </tr>
+                </table>
+                <p style="font-size:12px;line-height:23px;margin:0;color:#000;font-weight:800;letter-spacing:0;margin-top:20px;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;text-align:center;text-transform:uppercase">Powered by Aquamet.</p>
+            </body>
+
+        </html>
           `;
   },
 };
