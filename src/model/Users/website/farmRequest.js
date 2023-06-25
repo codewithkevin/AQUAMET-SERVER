5393d333ec0fd21b5b8116cd7bf54d7567a909cd
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-25 17:25:42 +0000

    update

--- src/model/Users/website/farmRequest.js
+++ src/model/Users/website/farmRequest.js
@@ -2,74 +2,82 @@ import mongoose from "mongoose";
 import Joi from "joi";
 import dotenv from "dotenv";
 
-const newFarmRequest = new mongoose.Schema({
-  fullName: {
-    type: String,
-    minLength: 2,
-    maxLength: 250,
-    required: true,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-  },
-  age: {
-    type: Number,
-    required: true,
-    minlength: 1,
-    maxlength: 1255,
-  },
-  idtype: {
-    type: String,
-    required: true,
-  },
-  idnumber: {
-    type: String,
-    required: true,
-    minLength: 5,
-    maxLength: 255,
-  },
-  location: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 1255,
-  },
-  farmlocationInfo: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 7980,
-  },
-  farmFacilities: {
-    type: [
-      {
-        type: String,
-        required: true,
+const newFarmRequest = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      minLength: 2,
+      maxLength: 250,
+      required: true,
+      maxlength: 50,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      minlength: 1,
+      maxlength: 1255,
+    },
+    idtype: {
+      type: String,
+      required: true,
+    },
+    idnumber: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 255,
+    },
+    location: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 1255,
+    },
+    farmlocationInfo: {
+      type: String,
+      minlength: 1,
+      maxlength: 7980,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    farmFacilities: {
+      type: [
+        {
+          type: String,
+          required: true,
+        },
+      ],
+      validate: {
+        validator: function (array) {
+          return array.length >= 1 && array.length <= 5;
+        },
+        message: "farmFacilities must have at least 1 and at most 5 elements",
       },
-    ],
-    required: true,
-    validate: {
-      validator: function (array) {
-        return array.length >= 1 && array.length <= 5;
-      },
-      message: "farmFacilities must have at least 1 and at most 5 elements",
+    },
+    meetingVenue: {
+      type: String,
+      required: true,
     },
   },
-  meetingVenue: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const FarmRequest = mongoose.model("newFarmRequest", newFarmRequest);
 
