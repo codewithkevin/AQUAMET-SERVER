e04767e26c8f48e92e17fadf2f74c238ca4a2f40
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-28 17:56:11 +0000

    added Phonenumber to create user webaccount

--- src/controllers/Users/website/usersController.js
+++ src/controllers/Users/website/usersController.js
@@ -11,7 +11,9 @@ const createUserAccount = async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send({ message: "User already registered" });
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user = new User(
+    _.pick(req.body, ["name", "email", "password", "phoneNumber"])
+  );
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
