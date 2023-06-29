85d1e44a5e3f6bbdae954214f2f2965df4998bb8
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-29 04:55:56 +0000

    added Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-const extraOrigins = [];
-if (process.env.NODE_ENV !== "production")
-  extraOrigins.push("http://localhost:5000", "http://localhost:4000");
+// const extraOrigins = [];
+// if (process.env.NODE_ENV !== "production")
+//   extraOrigins.push("http://localhost:5000", "http://localhost:4000");
 
-const originsWhitelist = [...extraOrigins, "http://aquamet.onrender.com"];
+// const originsWhitelist = [...extraOrigins, "http://aquamet.onrender.com"];
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // Create a new Winston logger instance
 const logger = winston.createLogger({
