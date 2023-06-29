1e5b20ea97756585b605b83de50b6867456ed9b4
Kelvin Osei <oseikelvin709@gmail.com>
2023-06-29 07:37:15 +0000

    update

--- src/model/Users/website/demo.js
+++ src/model/Users/website/demo.js
@@ -6,24 +6,20 @@ const demoSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    minlength: 5,
     maxlength: 50,
   },
   lastName: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 255,
+    maxlength: 50,
   },
   email: {
     type: String,
     required: true,
-    minlength: 5,
     maxlength: 255,
   },
   phoneNumber: {
     type: String,
-    required: true,
     minlength: 5,
     maxlength: 255,
   },
@@ -33,10 +29,10 @@ const Demo = mongoose.model("demoRequest", demoSchema);
 
 function validateDemo(user) {
   const schema = Joi.object({
-    firstName: Joi.string().min(5).max(50).required(),
-    lastName: Joi.string().min(2).max(255).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    phoneNumber: Joi.string().min(5).max(255).required(),
+    firstName: Joi.string().max(50).required(),
+    lastName: Joi.string().max(50).required(),
+    email: Joi.string().max(255).required().email(),
+    phoneNumber: Joi.string().max(255),
   });
 
   return schema.validate(user);
